73ee6badfe7ed91a64a725a2ce9e379fa7be229c
Romain Dorier <rdorier39@gmail.com>
2025-12-08 21:09:26 +0100

    use cmake to easily share project

---
+++ src/ExpressionDetector.hpp
@@ -0,0 +1,73 @@
+#pragma once
+
+#include <QtWidgets/QMainWindow>
+#include <QtWidgets/QWidget>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QRadioButton>
+#include <QVBoxLayout>
+#include <QHBoxLayout>
+#include <QPainter>
+#include <QTimer>
+#include <QFile>
+#include <QDir>
+#include <opencv2/core/core.hpp>
+#include <vector>
+#include "FaceDetection.hpp"
+#include "ImageManager.hpp"
+
+// enum representing the different modes of the expression detector
+enum DetectorMode {
+    NoDetect = 0, // no detection : the interface only display what camera sees
+    FaceDetect = 1, // face detection : the interface draw a figure around areas where faces are detected
+    ExpressionDetect = 2 // expression detection : the interface draw a figure around areas where faces are detected and display the name of the emotion
+};
+
+class ExpressionDetector : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    ExpressionDetector(QWidget *parent = nullptr);
+    ~ExpressionDetector();
+
+    // function connected to a timer to refresh frame display by interface in m_frameViewer
+    void updateFrame();
+    /*
+     * Change mode of the application (DetectorMode)
+     * This function is connected to radio buttons for user to choose mode with interface.
+     * @param button, a pointer to the QRadioButton that launch the function.
+     * @param checked, a boolean to tell if button is checked or not.
+     */
+    void modeChanged(QRadioButton* button, bool checked);
+    // return recommended size for the application
+    QSize sizeHint() const;
+    /*
+    * Draw rectangle on a given image.
+    * @param image: the image to draw rectangle on, a QImage.
+    * @param x : the x-coordinates of the upper left corner point of the rectangle to draw, an integer.
+    * @param y : the y-coordinates of the upper left corner point of the rectangle to draw, an integer.
+    * @param width, the width of the rectangle to draw, an integer.
+    * @param height, the height of the rectangle to draw, an integer.
+    */
+    void drawRectangle(QImage& image, int x, int y, int width, int height);
+
+private:
+    // mode the application is currently in
+    DetectorMode m_mode;
+    // face detection engine used to detect faces and expressions
+    FaceDetection m_faceDetect;
+    // timer to refresh viewer
+    QTimer* m_refreshTimer;
+
+    // members storing interface elements
+    QWidget* m_mainWidget;
+    QLabel* m_frameViewer; // viewer displaying camera stream with detection features
+    QHBoxLayout* m_mainLayout;
+    QVBoxLayout* m_detectionOptionsLayout;
+
+    // radio buttons to set one of the three modes of the application
+    QRadioButton* m_noDetectionOptionBtn;
+    QRadioButton* m_faceDetectionOptionBtn;
+    QRadioButton* m_expDetectionOptionBtn;
+};
+
---
+++ src/FaceDetection.cpp
@@ -0,0 +1,92 @@
+#include "FaceDetection.hpp"
+
+using namespace std;
+using namespace cv;
+
+FaceDetection::FaceDetection() : m_drawColor(255, 255, 255), m_faceCascadeDetector("data/haarcascades/haarcascade_frontalface_alt.xml")
+{
+	// Open the default camera (device ID 0)
+	int deviceID = 0;
+	int apiID = cv::CAP_ANY; // Auto-detect the backend API
+
+	m_capture.open(deviceID, apiID);
+}
+
+FaceDetection::~FaceDetection()
+{
+	// clear memory
+	m_capture.release();
+	cv::destroyAllWindows();
+}
+
+void FaceDetection::faceDetectionLoop()
+{
+	// store resulting image to display
+	Mat frame;
+
+	// store ASCII code of key pressed by user
+	int keyPressed = -1;
+
+
+	// capture loop
+	while (true) {
+		// capture frame from camera
+		bool result = m_capture.read(frame);
+		// check if resulting image is not empty and end capturing loop if so
+		if (result == false || frame.empty()) {
+			break;
+		}
+
+		try {
+			// use cascade classifier to detect face
+			vector<Rect> faceObjects = detectFaces(frame);
+			// draw rectangle to show faces detected
+			vector<Rect>::iterator faceIt;
+			for (faceIt = faceObjects.begin(); faceIt != faceObjects.end(); ++faceIt) {
+				rectangle(frame, *faceIt, m_drawColor);
+			}
+		}
+		catch (Exception const& e) {
+			cerr << "ERREUR : " << e.what() << endl;
+		}
+
+		// display frame
+		imshow("Expression Detection", frame);
+
+		// check if user has pressed escap key to stop capturing loop
+		keyPressed = waitKey(30);
+		if (keyPressed == 27) {
+			break;
+		}
+	}
+}
+
+std::vector<cv::Rect> FaceDetection::detectFaces(Mat const& image)
+{
+	// vector storing all area where faces are detected
+	vector<Rect> faceObjects;
+	if (image.empty())
+	{
+		throw invalid_argument("Empty image given !");
+	}
+	else {
+		// convert frame to grayscale for better result in face detection
+		Mat grayscaleFrame(image);
+		cvtColor(image, grayscaleFrame, COLOR_BGR2GRAY);
+
+		// use cascade classifier to detect face
+		m_faceCascadeDetector.detectMultiScale(grayscaleFrame, faceObjects);
+	}
+
+	return faceObjects;
+}
+
+cv::Mat FaceDetection::getCurrentFrame()
+{
+	// store resulting image to display
+	Mat frame;
+	// capture frame from camera
+	m_capture.read(frame);
+
+	return frame;
+}
