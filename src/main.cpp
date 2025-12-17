b31fe13b4e3fd7a6510dd0110230cd13fb421b0f
Romain Dorier <rdorier39@gmail.com>
2025-12-17 22:35:26 +0100

    manage application exceptions in main + add comment

--- src/main.cpp
+++ src/main.cpp
@@ -1,14 +1,25 @@
 #include <QtWidgets/QApplication>
+#include <QDebug>
 #include "ExpressionDetector.hpp"
 #include "FaceDetection.hpp"
 
 
 int main(int argc, char *argv[])
 {
-    QApplication app(argc, argv);
-    ExpressionDetector w;
+    /*
+    * Entry point for ExpressionDetector application.
+    * Initialize the QApplication et launch the window
+    */
+    try {
+        QApplication app(argc, argv);
+        ExpressionDetector w;
 
-    w.show();
-    
-    return app.exec();
+        w.show();
+
+        return app.exec();
+    }
+    catch (const std::exception& e) {
+        qCritical() << e.what();
+        return EXIT_FAILURE;
+    }
 }
