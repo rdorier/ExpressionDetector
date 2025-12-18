75f3b1503f6a3ac578b35e3dd1b867948d81ebd3
Romain Dorier <rdorier39@gmail.com>
2025-12-18 23:52:43 +0100

    improve convertOpencvToQImage function

--- src/ImageManager.cpp
+++ src/ImageManager.cpp
@@ -2,9 +2,11 @@
 
 QImage ImageManager::convertOpencvImageToQImage(cv::Mat const& imageToConvert)
 {
-    cv::Mat resultImg(imageToConvert);
+    cv::Mat resultImg;
     // opencv images are written in BGR colorspace, but Qt handle RGB colorspace, so we convert input image into the RGB one
     cv::cvtColor(imageToConvert, resultImg, cv::COLOR_BGR2RGB);
-
-    return QImage((uchar*)resultImg.data, resultImg.cols, resultImg.rows, resultImg.step, QImage::Format_RGB888);
+    // convert image from OpenCV into a QImage
+    QImage img = QImage((uchar*)resultImg.data, resultImg.cols, resultImg.rows, resultImg.step, QImage::Format_RGB888);
+    // make a deep copy so QImage do not depend on local variable resultImg
+    return img.copy();
 }
